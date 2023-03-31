18747610b0479a90ffeb79e10c8bc4cc148a162a
Archie Baer <archie@abaer.dev>
2023-03-31 02:09:09 +0100

    sign in card

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 import { SidebarStreamer } from "./SidebarStreamer"
 
 export const Sidebar = () => {
-	let { data: profile } = trpc.profile.useQuery()
+	let { data: profile, isLoading: profileLoading } = trpc.profile.useQuery()
 	let { data: activity } = trpc.activity.useQuery()
 
 	return (
@@ -29,23 +29,40 @@ export const Sidebar = () => {
 				)}
 			</div>
 
-			{profile && (
-				<div className="flex items-center space-x-2 p-4">
-					{profile.avatar ? (
-						<img
-							src={`https://files.glaffle.com/avatars/${encodeURIComponent(
-								profile.id
-							)}/${encodeURIComponent(profile.avatar)}/128.png`}
-							alt=""
-							className="w-8 h-8 bg-neutral-700 rounded-md"
-						/>
-					) : (
-						<div className="w-8 h-8 bg-emerald-400 rounded-md" />
-					)}
+			{!profileLoading &&
+				(profile ? (
+					<div className="flex items-center space-x-2 p-4">
+						{profile.avatar ? (
+							<img
+								src={`https://files.glaffle.com/avatars/${encodeURIComponent(
+									profile.id
+								)}/${encodeURIComponent(profile.avatar)}/128.png`}
+								alt=""
+								className="w-8 h-8 bg-neutral-700 rounded-md"
+							/>
+						) : (
+							<div className="w-8 h-8 bg-emerald-400 rounded-md" />
+						)}
 
-					<p className="text-lg">{profile.username}</p>
-				</div>
-			)}
+						<p className="text-lg">{profile.username}</p>
+					</div>
+				) : (
+					<div className="bg-neutral-700 rounded-md m-2 p-4 space-y-4">
+						<div className="space-y-1">
+							<p className="text-lg font-medium">Find Your People</p>
+							<p className="text-xs text-neutral-400">
+								Join the community with a Glaffle account - no password needed.
+							</p>
+						</div>
+
+						<Link
+							href="/auth"
+							className="block py-1 text-center rounded-md bg-emerald-600 text-white"
+						>
+							Sign in with Google
+						</Link>
+					</div>
+				))}
 		</div>
 	)
 }
